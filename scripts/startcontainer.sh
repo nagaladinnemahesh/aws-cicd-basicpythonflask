864f9d819dc4100663fc525137612186bb54eb6f
Mahesh Nagaladinne <130373868+nagaladinnemahesh@users.noreply.github.com>
2023-10-05 20:02:56 +0530

    Update startcontainer.sh

--- scripts/startcontainer.sh
+++ scripts/startcontainer.sh
@@ -3,6 +3,5 @@ set -e
 
 # Pull the Docker image from Docker Hub
 docker pull mahesh0404/python-flask-cicd
-
 # Run the Docker image as a container
 docker run -d -p 5000:5000 mahesh0404/python-flask-cicd
