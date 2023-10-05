d66c16bc6f9d68638d25237f916aaa0fddfd8bec
Mahesh Nagaladinne <130373868+nagaladinnemahesh@users.noreply.github.com>
2023-10-05 23:12:44 +0530

    Update app.py
    removed ' from i'm causing error while runing a docker image

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world! I'm Mahesh. Deployment is not successfull'
+    return 'Hello, world! Im Mahesh. Deployment is not successfull'
 
 if __name__ == '__main__':
     app.run()
