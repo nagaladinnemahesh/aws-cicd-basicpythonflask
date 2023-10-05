10dd09ab03b9b99663b3734595b467cae0aad5ee
Mahesh Nagaladinne <130373868+nagaladinnemahesh@users.noreply.github.com>
2023-10-05 19:42:22 +0530

    Create startcontainer.sh

---
+++ scripts/startcontainer.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+# Pull the Docker image from Docker Hub
+docker pull mahesh0404/python-flask-cicd
+
+# Run the Docker image as a container
+docker run -d -p 5000:5000 mahesh0404/python-flask-cicd
