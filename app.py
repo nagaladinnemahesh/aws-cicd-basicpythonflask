cfbaf1864dbdc85eb7b6c327b0499e448543ca8c
Mahesh Nagaladinne <130373868+nagaladinnemahesh@users.noreply.github.com>
2023-10-06 00:20:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,10 @@
-# from flask import Flask
-
-# app = Flask(__name__)
-
-# @app.route('/')
-# def hello():
-#     return 'Hello, world! Im Mahesh. Deployment is not successfull'
-
-# if __name__ == '__main__':
-#     app.run()
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world! Im Mahesh. Deployment is successful'
+    return 'Hello, world! Im Mahesh. Deployment is not successfull'
 
 if __name__ == '__main__':
-    app.run
-
+    app.run()
